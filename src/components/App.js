7d00fe0b7cabce67c7794424325bba1a82c48155
NouhRastanawi <noahrastdark@gmail.com>
2022-06-02 20:36:41 +0300

    Done

--- src/components/App.js
+++ src/components/App.js
@@ -1,7 +1,20 @@
 import React from "react";
+import AlbumCard from "./AlbumCard";
+import AlbumList from "./AlbumList";
+import Logo from "./Logo.js";
+import TopMenu from "./TopMenu.js";
+import SideMenu from "./SideMenu.js";
+import GenrePicker from "./GenrePicker.js";
+import MainContent from "./MainContent.js";
 
 function App() {
-  return <div className="app-container">{/* What components go here? */}</div>;
+  return (
+    <div className="app-container">
+      <TopMenu/>
+      <MainContent/>
+      <SideMenu/>
+    </div>
+  );
 }
 
 export default App;
--- src/components/SideMenu.js
+++ src/components/SideMenu.js
@@ -1,8 +1,15 @@
 import React from "react";
+import Search from "./Search.js";
+import Logo from "./Logo.js";
+import MenuLinks from "./MenuLinks.js";
 
 function SideMenu() {
   return (
-    <aside className="side-bar open">{/* What components go here? */}</aside>
+    <aside className="side-bar open">
+      <Logo/>
+      <MenuLinks/>
+      <Search/>
+      </aside>
   );
 }
 
